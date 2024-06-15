a2531acf27353a71adaf2a026d11aaa347d5ad7d
Alvin widinugroho <alvinwidinugroho@gmail.com>
2024-06-15 20:30:34 +0700

    benerin filter

--- hasil_pertanian/urls.py
+++ hasil_pertanian/urls.py
@@ -4,7 +4,7 @@ from hasil_pertanian.views import *
 
 urlpatterns = [
     path('add-hasil-pertanian/',add_hasil_pertanian),
-    path('update-hasil-pertanian/',update_hasil_pertanian),
+    path('update-hasil-pertanian/<id>/',update_hasil_pertanian),
     path('get-hasil-pertanian/',get_list_hasil_pertanian),
     path('get-list-pertanian-daerah/<daerah>/<jenis_tanaman>/',get_list_hasil_pertanian_daerah)
 ]
