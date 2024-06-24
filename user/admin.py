af47b29ac0632ec1c3f212ac5a229b28ad9301ef
Alvin widinugroho <alvinwidinugroho@gmail.com>
2024-06-24 15:10:33 +0700

    benerin

--- user/admin.py
+++ user/admin.py
@@ -3,7 +3,7 @@ from user.models import User,Pemerintah,Petani,Admin
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    fields = ('username','name','email','role','daerah','datetime_created')
+    fields = ('username','name','email','role','daerah')
     list_display = ("user_id",'username','name','email','role','daerah','datetime_created')
 
 @admin.register(Pemerintah)
